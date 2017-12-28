aa827f1f38b5f80cda3348460f1632fa9dbcd68e
Sujit Thonse <thonse@stud.fra-uas.de>
2017-12-28 03:07:59 +0100

    Changes made in the main controller to get the graph change as per MiniPID class.

--- ABS Simulator/src/application/MainController.java
+++ ABS Simulator/src/application/MainController.java
@@ -39,11 +39,30 @@ public class MainController extends VehicleSpeed{
 		
 		
 	}
+	//MiniPID class is used to simulate acceleration temperoraly. This will be replaced with model when the 'model class' is programed by Panda.
 	public void gas(ActionEvent event) {
+		MiniPID miniPID;
+		miniPID = new MiniPID(0.25, 0.01, 0.4);
+		miniPID.setOutputLimits(10);
+		miniPID.setMaxIOutput(2);
+		miniPID.setOutputRampRate(3);
+		miniPID.setOutputFilter(.3);
+		double target=100;
+		double actual=0;
+		double output=0;
+		
+		miniPID.setSetpoint(0);
+		miniPID.setSetpoint(target);
+		
+		
 		XYChart.Series<String, Number> series =new XYChart.Series<String, Number>();
 		for (int i = 0 , j = 0 ; i < 100; i++ ,j++){
-			String strj = Integer.toString(j);
-			series.getData().add(new XYChart.Data<String, Number>(strj, i));
+			
+			output = miniPID.getOutput(actual, target);
+			actual = actual + output;
+			
+			String strj = Integer.toString(j);//this is not used for the timebeing.
+			series.getData().add(new XYChart.Data<String, Number>(strj, actual));
 			
 		
 		lineChart.getData().addAll(series);
@@ -54,16 +73,34 @@ public class MainController extends VehicleSpeed{
 	
 	
 	public void brake(ActionEvent event) {
-		XYChart.Series<String, Number> series =new XYChart.Series<String, Number>();
-		series.getData().add(new XYChart.Data<String, Number>("800", 200));
-		series.getData().add(new XYChart.Data<String, Number>("900", 500));
-		series.getData().add(new XYChart.Data<String, Number>("700", 600));
-		series.getData().add(new XYChart.Data<String, Number>("400",300));
-		series.getData().add(new XYChart.Data<String, Number>("600", 200));
-		series.getData().add(new XYChart.Data<String, Number>("100", 200));
+		MiniPID miniPID;
+		miniPID = new MiniPID(0.25, 0.01, 0.4);
+		miniPID.setOutputLimits(10);
+		miniPID.setMaxIOutput(2);
+		miniPID.setOutputRampRate(3);
+		miniPID.setOutputFilter(.3);
+		double target=0;
+		double actual=100;
+		double output=0;
 		
+		miniPID.setSetpoint(0);
+		miniPID.setSetpoint(target);
+		
+		
+		XYChart.Series<String, Number> series =new XYChart.Series<String, Number>();
+		for (int i = 0 , j = 100 ; i < 100; i++ ,j++){
+			
+			output = miniPID.getOutput(actual, target);
+			actual = actual + output;
+			
+			String strj = Integer.toString(j);//this is not used for the timebeing.
+			series.getData().add(new XYChart.Data<String, Number>(strj, actual));
+			
 		
 		lineChart.getData().addAll(series);
+			
+		}
+		
 		
 		
 	}
